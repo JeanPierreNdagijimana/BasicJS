8fc5ef1c328e54f04cd8534a1040a16eb5d530cf
Jean Pierre Ndagijimana(PRIVILEGED) <prv_jndagijimana@innocap.com>
2023-05-23 15:55:12 +0200

    Display data from JS file to HTML page

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const champ = {
-    fn: "john",
-    ln: "doe",
-};
-// console.log(champ.fn);
+// const champ = {
+//     fn: "john",
+//     ln: "doe",
+// };
+// // console.log(champ.fn);
 
-const arr = ['jane', 'doe', 10]
-// console.log(arr[1]);
-const test = {
-    student: {
-        fname: 'juba',
-        lname: 'kampala',
-        city: 'kigali',
-    },
-}
-console.log(`student name is ${test.student.fname} and lives in ${test.student.city}`);
-const test2 = {
+// const arr = ['jane', 'doe', 10]
+// // console.log(arr[1]);
+// const test = {
+//     student: {
+//         fname: 'juba',
+//         lname: 'kampala',
+//         city: 'kigali',
+//     },
+// }
+// console.log(`student name is ${test.student.fname} and lives in ${test.student.city}`);
+let test2 = {
     "success": true,
     "data": [
         {
@@ -47,4 +47,29 @@ const test2 = {
         }
     }
 }
-console.log(test2.data[0].firstName);
+let NewTest2 = test2;
+NewTest2.data[0].firstName = 'Jean';
+NewTest2.data[0].lastName = 'Ndagijimana';
+
+for( let i=0; i<test2.data.length; i++){
+    console.log(`Student studentID: ${test2.data[i].studentId}, Student first name: ${test2.data[i].firstName}, Student last name: ${test2.data[i].lastName}, Student email: ${test2.data[i].email}, Student phone: ${test2.data[i].phone}, Student city: ${test2.data[i].city}, Student Created at: ${test2.data[i].createdAt}, Student updated at: ${test2.data[i].updatedAt}`);
+}
+
+let studentTable = document.querySelector('#student-info');
+
+for (let i = 0; i < test2.data.length; i++) {
+    let student = test2.data[i];
+    let row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${student.id}</td>
+        <td>${student.studentId}</td>
+        <td>${student.firstName}</td>
+        <td>${student.lastName}</td>
+        <td>${student.email}</td>
+        <td>${student.phone}</td>
+        <td>${student.city}</td>
+        <td>${student.createdAt}</td>
+        <td>${student.updatedAt}</td>
+    `;
+    studentTable.appendChild(row);
+}
